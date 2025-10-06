3adb942cd426068e24d71c99e2dc6db7a32c1cf9
NaveenGuttedar <naveen.g@wardenera.com>
2025-10-06 16:10:57 +0530

    Delete dist directory

--- dist/src/backend/database/prisma.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const client_1 = require("@prisma/client");
-const prismaClientSingleton = () => {
-    return new client_1.PrismaClient();
-};
-const prisma = prismaClientSingleton();
-exports.prisma = prisma;
